bca398d4025ab80e5c1ad03090ead1aa070cbdc0
Serhii Vernii <SergVerny95@gmail.com>
2024-08-14 23:06:56 +0300

    Create quest.js

---
+++ quest.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Question(props) {
+    
+    return (
+        <div className='row question'>
+            <p>{props.title}</p>
+        </div>
+    )
+
+}
+export default Question
